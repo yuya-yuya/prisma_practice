9991f99d0e1fd741dd543cd8035b738d94e3d352
yuya-yuya <yytyfp5115@icloud.com>
2023-03-08 20:29:17 +0900

    タイプミス修正

--- src/script.js
+++ src/script.js
@@ -3,12 +3,12 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient();
 
 async function main() {
-  const newLink = await prisma.link.create({
-    date: {
-      description: "description_test",
-      url: "www.test.com"
-    }
-  });
+  // const newLink = await prisma.link.create({
+  //   data: {
+  //     description: "description_test",
+  //     url: "www.test.com"
+  //   }
+  // });
   const allLinks = await prisma.link.findMany();
 }
 
